63fa304725ca5ba438a496bd5b67c93718b83183
Jaime <jgomezm@tecsup.edu.pe>
2025-04-16 15:37:09 -0500

    Initial Commit

---
+++ apple_lab06_04_CustomNavigationBarTests/apple_lab06_04_CustomNavigationBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  apple_lab06_04_CustomNavigationBarTests.swift
+//  apple_lab06_04_CustomNavigationBarTests
+//
+//  Created by developer on 4/16/25.
+//
+
+import Testing
+@testable import apple_lab06_04_CustomNavigationBar
+
+struct apple_lab06_04_CustomNavigationBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
