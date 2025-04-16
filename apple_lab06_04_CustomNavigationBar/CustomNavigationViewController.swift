6b9431e3259a4af486d2d3588a6ccf29b7d4de5d
Jaime <jgomezm@tecsup.edu.pe>
2025-04-16 15:58:26 -0500

    first commit

---
+++ apple_lab06_04_CustomNavigationBar/CustomNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomNavigationViewController.swift
+//  apple_lab06_04_CustomNavigationBar
+//
+//  Created by developer on 4/16/25.
+//
+
+import UIKit
+
+class CustomNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
